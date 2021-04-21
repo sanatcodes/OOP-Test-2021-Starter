4f945f4c1309c85e9ef7d2624a064616f8fce550
sanatcodes <sanat.thukral@gmail.com>
2021-04-21 17:09:24 +0100

    Print score function working

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -27,15 +27,27 @@ public class ScoreDisplay extends PApplet
 			//if note duration is 2 
 			if(Character.isDigit(n1))
 			{
-				Note s = new Note(n,1);
+				Note s = new Note(n,2);
 				notes.add(s);
 				i++;
 			}
 			else{
-				Note s = new Note(n,2);
+				Note s = new Note(n,1);
 				notes.add(s);
 			}
+		}// end for 
+	}// end load score
 
+	public void printScore(String score){
+
+		for(Note n : notes)
+		{
+			if(n.getDuration() == 1)
+			{
+				System.out.println(n.getNote() + "\t" + n.getDuration() + "\t" + "Quaver " );
+			}else {
+				System.out.println(n.getNote() + "\t" + n.getDuration() + "\t" + "Crotchet" );
+			}
 			
 		}
 	}
@@ -45,14 +57,15 @@ public class ScoreDisplay extends PApplet
 		size(1000, 500);
 
 		// How to convert a character to a number
-		char c = '7'; // c holds the character 7 (55)
-		int i = c - '0'; // i holds the number 7 (55 - 48) 
-		println(i);
+		// char c = '7'; // c holds the character 7 (55)
+		// int i = c - '0'; // i holds the number 7 (55 - 48) 
+		// println(i);
 	}
 
 	public void setup() 
 	{
 		loadScore(score);
+		printScore(score);
 	}
 
 	public void draw()
